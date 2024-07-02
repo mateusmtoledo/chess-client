c85adfade28b3c157e987d3370de256b79d45388
mateusmtoledo <mateusmtoledo1@gmail.com>
2024-07-02 19:43:14 -0300

    Add NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <script setup lang="ts">
-import { Puzzle, Settings, Swords, User } from 'lucide-vue-next';
-import NavLink from './components/NavLink.vue';
+import NavBar from './components/NavBar.vue';
 import GamePage from './pages/GamePage.vue';
 </script>
 
 <template>
   <div class="grid grid-cols-[240px_1fr] h-screen bg-muted/40">
-    <div class="bg-background border-r border-border flex flex-col gap-4 p-4">
-      <h1 class="text-3xl font-extrabold">
-        MChess
-      </h1>
-      <nav className="flex flex-col gap-2">
-        <NavLink text="Profile" :icon="User" />
-        <NavLink text="Games" :icon="Swords" />
-        <NavLink text="Puzzles" :icon="Puzzle" />
-        <NavLink text="Settings" :icon="Settings" />
-      </nav>
-    </div>
+    <NavBar />
     <main class="p-8">
       <GamePage />
     </main>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Puzzle, Settings, Swords, User } from 'lucide-vue-next';
+import NavLink from './NavLink.vue';
+</script>
+
+<template>
+  <div class="bg-background border-r border-border flex flex-col justify-between p-4">
+    <div>
+      <h1 class="text-3xl font-extrabold m-4">
+        MChess
+      </h1>
+      <nav className="flex flex-col gap-2">
+        <NavLink text="Profile" :icon="User" />
+        <NavLink text="Games" :icon="Swords" />
+        <NavLink text="Puzzles" :icon="Puzzle" />
+        <NavLink text="Settings" :icon="Settings" />
+      </nav>
+    </div>
+    <div>
+    </div>
+  </div>
+</template>
