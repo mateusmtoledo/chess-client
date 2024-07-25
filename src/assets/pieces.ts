065a92b8085be51ac3242707670d2b01610e8cd9
mateusmtoledo <mateusmtoledo1@gmail.com>
2024-07-25 18:35:27 -0300

    Fix piece filenames

--- src/assets/pieces.ts
+++ src/assets/pieces.ts
@@ -13,7 +13,7 @@ type PieceSet = {
 const pieces: PieceSet = {}
 _pieces.forEach((piece) => {
   if (typeof piece !== 'string') return
-  const filename = piece.match(/\/([^/]+)\.svg$/)?.[1]
+  const filename = piece.match(/\/([^/]+)\.svg$/)?.[1].split('-')[0]
   if (!filename) return
   pieces[filename] = piece
 })
